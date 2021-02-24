8f7c5e7350faa7d4a6e232087d2fa5b828444902
Aaron Hall <c.aaron.hall@gmail.com>
2021-02-24 17:30:37 -0600

    Moved reading subscribed messsages to it's own function Using topic enums Reading/displaying temp and relay state

--- thermo.py
+++ thermo.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 import zmq
-
+from enums import RelayState,Topics,Ports
 
 qtCreatorFile = "gui.ui" # Enter file here.
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
@@ -13,13 +13,13 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
         self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.SUB)
-        #setup receiving set temp socket
-        self.socket.connect("tcp://10.0.0.154:%s" % 5558)
+        self.rcvsocket = self.context.socket(zmq.SUB)
+        #setup receiving set temp socket (publish port on the rpi side)
+        self.rcvsocket.connect("tcp://10.0.0.154:%s" % Ports.PUBLISH_PORT.value)
 
-        self.socket.subscribe("")
+        self.rcvsocket.subscribe("")
         self.poller = zmq.Poller()
-        self.poller.register(self.socket, zmq.POLLIN)
+        self.poller.register(self.rcvsocket, zmq.POLLIN)
 
         self.temperature = 0
         self.setpoint = 38
@@ -32,22 +32,33 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.timer.timeout.connect(self.update_display)
         self.timer.start(1000)  # every 1000 milliseconds
 
+    def get_msgs(self):
+        topic, data = None, None
+        active_socks = dict(self.poller.poll(timeout=10))
+        if self.rcvsocket in active_socks and active_socks[self.rcvsocket] == zmq.POLLIN:
+            active_socks = dict(self.poller.poll(timeout=10))
+            if self.rcvsocket in active_socks and active_socks[self.rcvsocket] == zmq.POLLIN:
+                msg = self.rcvsocket.recv()
+                topic = int(msg)
+                data = self.rcvsocket.recv()
+        return topic, data
+
 
     def update_display(self):
-        active_socks = dict(self.poller.poll(timeout=10))
-        if self.socket in active_socks and active_socks[self.socket] == zmq.POLLIN:
-            while self.socket in active_socks and active_socks[self.socket] == zmq.POLLIN:
-                msg = self.socket.recv()
-                #print(msg)
-                if int(msg) == 3:       # temperature
-                    data = self.socket.recv()
-                    temp = "%.1f" % float(data)
-                    self.lcdNumber_2.display(temp)
-                elif int(msg) == 4:
-                active_socks = dict(self.poller.poll(timeout=10))
+        topic, data = self.get_msgs()
+        while topic is not None:
+            if topic == Topics.TEMP.value:       # temperature
+                temp = "%.1f" % float(data)
+                self.lcdNumber_2.display(temp)
+            elif topic == Topics.RELAY_STATE.value:
+                s = "ON" if int(data) == 1 else "OFF"
+                self.label_relaystate.setText("Relay:" + str(s))
+
+            topic, data = self.get_msgs()
 
         self.lcdNumber.display("%.1f" % self.setpoint)
-        #self.results_output.setText(total_price_string)
+
+
 
     def sp_up_pressed(self):
         self.setpoint += 1
