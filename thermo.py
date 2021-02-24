e265fee79f192cc0e87fb5be1affe9ac7843a059
Aaron Hall <c.aaron.hall@gmail.com>
2021-02-24 15:23:32 -0600

    Moved main file to thermo.py

--- thermo.py
+++ thermo.py
@@ -1,83 +1,65 @@
-import signal
 import sys
-from enum import Enum
-from time import sleep
-import threading
-from keezer import keezer
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import zmq
 
 
-class RelayState(Enum):
-    ON = 1
-    OFF = 0
+qtCreatorFile = "gui.ui" # Enter file here.
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 
-signal_exit = False
-
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    signal_exit = True
-
-signal.signal(signal.SIGINT, signal_handler)
-
-class therm_service():
+class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
-        self.keezer = keezer()
-        self.signal_exit = False
-        self.ok_to_switch = False
-        self.relay_state = RelayState.OFF
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.SUB)
+        #setup receiving set temp socket
+        self.socket.connect("tcp://10.0.0.154:%s" % 5558)
 
-    def start(self):
-        self.start_protection_timer()
-        self.main()
+        self.socket.subscribe("")
+        self.poller = zmq.Poller()
+        self.poller.register(self.socket, zmq.POLLIN)
 
-    def stop(self):
-        self.signal_exit = True
+        self.temperature = 0
+        self.setpoint = 38
+        self.lcdNumber.display("%.1f" % self.setpoint)
 
-    def protection_timer_handler(self):
-        self.ok_to_switch = True
-        self.relay_timer = None
+        self.pushButton_2.clicked.connect(self.sp_up_pressed)
+        self.pushButton.clicked.connect(self.sp_down_pressed)
 
-    def start_protection_timer(self):
-        threading.Timer(self.keezer.compressor_protection * 60.0, self.protection_timer_handler).start()
-        self.ok_to_switch = False
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.update_display)
+        self.timer.start(1000)  # every 1000 milliseconds
 
-    def relay_on(self):
-        self.relay_state = RelayState.ON
-        self.start_protection_timer()
 
-    def relay_off(self):
-        self.relay_state = RelayState.OFF
-        self.start_protection_timer()
+    def update_display(self):
+        active_socks = dict(self.poller.poll(timeout=10))
+        if self.socket in active_socks and active_socks[self.socket] == zmq.POLLIN:
+            while self.socket in active_socks and active_socks[self.socket] == zmq.POLLIN:
+                msg = self.socket.recv()
+                #print(msg)
+                if int(msg) == 3:       # temperature
+                    data = self.socket.recv()
+                    temp = "%.1f" % float(data)
+                    self.lcdNumber_2.display(temp)
+                elif int(msg) == 4:
+                active_socks = dict(self.poller.poll(timeout=10))
 
-    def relay_toggle(self):
-        self.relay_state = self.relay_state ^ 1
-        self.start_protection_timer()
+        self.lcdNumber.display("%.1f" % self.setpoint)
+        #self.results_output.setText(total_price_string)
 
-    def main(self):
-        while not self.signal_exit:
-            # update values received from sockets
-            if self.ok_to_switch:
-                if self.relay_state == RelayState.OFF:
-                    if self.keezer.temperature > (self.keezer.setpoint + self.keezer.hysteresis):
-                        self.relay_on()
-                else:   # relay is ON
-                    if self.keezer.temperature < (self.keezer.setpoint - self.keezer.hysteresis):
-                        self.relay_off()
-            else:
-                pass
+    def sp_up_pressed(self):
+        self.setpoint += 1
+        self.lcdNumber.display("%.1f" % self.setpoint)
 
-        # decide if switch will happen
-        # if switch value changed, restart relay timer
-            sleep(1)
+    def sp_down_pressed(self):
+        self.setpoint -= 1
+        self.lcdNumber.display("%.1f" % self.setpoint)
+
 
 if __name__ == "__main__":
-    tservice = therm_service()
-    tservice.start()
-    #print any relevant messages
-    # wait for ctrl-C to stop
-    while (signal_exit is False):
-        sleep(1)
-
-    # do any needed cleanup
-    signal.pause()
-    tservice.stop()
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    sys.exit(app.exec_())
